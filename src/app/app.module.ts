9e9ce53b976fb05e64b902ae7bb1cd66ca36b2be
Ayşe Dilara Öztürk <adilaraozturk@gmail.com>
2024-06-25 11:00:50 +0300

    layout and some components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderMenuComponent } from './header-menu/header-menu.component';
+import { CategoryMenuComponent } from './category-menu/category-menu.component';
+import { ProjectListComponent } from './project-list/project-list.component';
+import { ProjectDetailComponent } from './project-detail/project-detail.component';
+import { TaskListComponent } from './task-list/task-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderMenuComponent,
+    CategoryMenuComponent,
+    ProjectListComponent,
+    ProjectDetailComponent,
+    TaskListComponent
   ],
   imports: [
     BrowserModule,
