61d5755b56020494330937c9d24e4101b5c0062d
Ayşe Dilara Öztürk <adilaraozturk@gmail.com>
2024-06-26 15:11:45 +0300

    creating services

---
+++ src/app/services/projects.service.ts
@@ -0,0 +1,32 @@
+import { HttpClient } from '@angular/common/http'
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Project } from '../models/project.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProjectsService {
+
+  constructor(private http: HttpClient) { }
+
+  url:'http://localhost:5263/api/Projects';
+
+  getProjects():Observable<Project[]>{
+    return this.http.get<Project[]>(this.url);
+  }
+
+  getProjectByCategory(categoryID: number):Observable<Project[]>{
+
+    return this.http.get<Project[]>(this.url+'/'+categoryID);
+  }
+
+  addProject(project: Project):Observable<Project>{
+  //  let option{
+  //      headers: {
+  //        'Authorization': 'Bearer [TOKEN EKLENECEK]'
+  //  }
+  // }
+    return this.http.post<Project>(this.url,project);
+  }
+}
