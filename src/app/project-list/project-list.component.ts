a3bb61e9a1e8dcea93e611c27c6f8bd5f6eaecb1
Ayşe Dilara Öztürk <adilaraozturk@gmail.com>
2024-06-26 12:58:46 +0300

    why we use observable

--- src/app/project-list/project-list.component.ts
+++ src/app/project-list/project-list.component.ts
@@ -18,8 +18,14 @@ export class ProjectListComponent  implements OnInit{
   }
 
   ngOnInit(): void {
-      this.route.params.subscribe((arg)=>{
+      this.route.params.subscribe((arg)=>{ 
+        //ActivatedRoute parametresinin params koleksiyonundaki bir değişikliğin karşı taraftaki bileşende de değişmesini istiyorsak bunun adı: Observer
+        // Birden fazla bileşenin koleksiyona abone olması durumunda bütün aboneler gerçekleşen değişikliklerden haberdar olurlar. Abone olma: Subscribe, Haberdar olmaları için giden bildirim: Notification
         console.log(arg['id']);
+        if (arg['id'] != undefined){
+          let id : number = Number.parseInt(arg['id']);
+          this.projects = projects.filter(pr => pr.categoryID == id);
+        }
       })
   }
 }
