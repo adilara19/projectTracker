54d50a9240f14f9e853177881f17a7ebded5c91c
Ayşe Dilara Öztürk <adilaraozturk@gmail.com>
2024-06-27 11:37:51 +0300

    adding option for chooseCategory via category.id

--- src/app/add-project/add-project.component.ts
+++ src/app/add-project/add-project.component.ts
@@ -1,18 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Project } from '../models/project.model';
 import { NgForm } from '@angular/forms';
+import { CategoriesService } from '../services/categories.service';
+import { Category } from '../models/category.model';
+import { ProjectsService } from '../services/projects.service';
 
 @Component({
   selector: 'app-add-project',
   templateUrl: './add-project.component.html',
   styleUrls: ['./add-project.component.css']
 })
-export class AddProjectComponent {
+export class AddProjectComponent implements OnInit{
+
+  constructor(private categoryService: CategoriesService, private projectService: ProjectsService){}
+
   project: Project = new Project();
+  categories: Category [];
+
+  ngOnInit(): void {
+      this.categoryService.getCategories()
+                          .subscribe((data: Category[])=>{
+                            this.categories = data;});
+  }
 
   add(form: NgForm){
     if(form.valid){
       console.log(form.value);
+      this.project = form.value;
+      this.projectService.addProject(this.project)
+                          .subscribe((incommingdata: Project)=>{
+                            console.log('$ {incommingData.name} başarıyla eklendi.')
+                          })
     }
   }
 
